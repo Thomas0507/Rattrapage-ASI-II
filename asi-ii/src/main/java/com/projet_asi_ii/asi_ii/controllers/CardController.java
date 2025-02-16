ce32dd9964bce659bca85e49bd5c8c4bf0e651e0
Thomas0507 <marin.f.thomas@gmail.com>
2025-02-16 21:32:59 +0100

    feat: wip completing crud interface for card

--- asi-ii/src/main/java/com/projet_asi_ii/asi_ii/controllers/CardController.java
+++ asi-ii/src/main/java/com/projet_asi_ii/asi_ii/controllers/CardController.java
@@ -4,9 +4,11 @@ import com.projet_asi_ii.asi_ii.dtos.CardDto;
 import com.projet_asi_ii.asi_ii.mappers.CardMapper;
 import com.projet_asi_ii.asi_ii.services.CardService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.net.http.HttpResponse;
 import java.util.List;
 
 @RestController
@@ -16,8 +18,12 @@ public class CardController {
     @Autowired
     CardService cardService;
 
-    @GetMapping(name = "/getAll", produces = "application/json")
+    @GetMapping(produces = "application/json")
     public ResponseEntity<List<CardDto>> getCards() {
         return ResponseEntity.ok(this.cardService.getCards());
+    }
+    @PostMapping
+    public ResponseEntity<Boolean> createCard(@RequestBody List<CardDto> cardsDto) {
+        return ResponseEntity.ok(this.cardService.insertCards(cardsDto));
     }
 }
