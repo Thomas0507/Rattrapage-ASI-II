2c565949f9631b9ac28e889001b5452a245cca40
Thomas0507 <marin.f.thomas@gmail.com>
2025-02-16 19:55:12 +0100

    fix: [BACK] - fix request mapping

--- asi-ii/src/main/java/com/projet_asi_ii/asi_ii/controllers/CardController.java
+++ asi-ii/src/main/java/com/projet_asi_ii/asi_ii/controllers/CardController.java
@@ -5,20 +5,18 @@ import com.projet_asi_ii.asi_ii.mappers.CardMapper;
 import com.projet_asi_ii.asi_ii.services.CardService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@RestController(value = "/cards")
+@RestController
+@RequestMapping("cards")
 public class CardController {
 
     @Autowired
     CardService cardService;
 
-
-    @GetMapping
+    @GetMapping(name = "/getAll", produces = "application/json")
     public ResponseEntity<List<CardDto>> getCards() {
         return ResponseEntity.ok(this.cardService.getCards());
     }
--- asi-ii/src/main/java/com/projet_asi_ii/asi_ii/controllers/TestController.java
+++ asi-ii/src/main/java/com/projet_asi_ii/asi_ii/controllers/TestController.java
@@ -2,12 +2,14 @@ package com.projet_asi_ii.asi_ii.controllers;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController("/")
+@RestController
+@RequestMapping("test")
 public class TestController {
 
-    @GetMapping("/test")
+    @GetMapping(name = "/test")
     public ResponseEntity<String>testFunction() {
         return ResponseEntity.ok("test");
     }
