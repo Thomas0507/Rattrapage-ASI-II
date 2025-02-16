e2bcdbcd234c976802f79bdce93d8391b17b1d37
Thomas0507 <marin.f.thomas@gmail.com>
2025-02-16 16:29:23 +0100

    feat: [FRONT] - Added tailwind, basic routing & component folder

---
+++ frontend/src/app/cards/[id]/page.tsx
@@ -0,0 +1,3 @@
+export default function CardById() {
+    return <h1>Card by Id</h1>
+}
--- frontend/tailwind.config.ts
+++ frontend/tailwind.config.ts
@@ -1,18 +1,22 @@
 import type { Config } from "tailwindcss";
 
+
 export default {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    // Or if using `src` directory:
+    "./src/app/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
+      fontFamily: {
+        sans: ['InterVariable', '...defaultTheme.fontFamily.sans'],
+      },
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
       },
     },
   },
-  plugins: [],
+  plugins: [
+  ],
 } satisfies Config;
