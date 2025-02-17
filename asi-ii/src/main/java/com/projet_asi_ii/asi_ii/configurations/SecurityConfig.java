3aeb0e13a040dd689fca534a40c62898544a0797
remi <remitvb@gmail.com>
2025-02-17 22:47:37 +0100

    feat: auth jwt and cors

--- asi-ii/src/main/java/com/projet_asi_ii/asi_ii/configurations/SecurityConfig.java
+++ asi-ii/src/main/java/com/projet_asi_ii/asi_ii/configurations/SecurityConfig.java
@@ -1,20 +1,23 @@
 package com.projet_asi_ii.asi_ii.configurations;
 
 import com.projet_asi_ii.asi_ii.services.UserDetailsServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.ProviderManager;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.annotation.web.configurers.LogoutConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import java.util.List;
@@ -22,20 +25,27 @@ import java.util.List;
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+	@Autowired
+	private JwtFilter jwtFilter;
+
 	@Bean
 	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 		http
+				.cors(cors -> corsConfigurationSource())
 				.authorizeHttpRequests(requests -> requests
 						.requestMatchers("/auth/**").permitAll()
 						.anyRequest().authenticated()
-				).csrf(AbstractHttpConfigurer::disable) // add custom csrf for unsafe methods
+				).csrf(AbstractHttpConfigurer::disable)
+				.httpBasic(Customizer.withDefaults())
 				.formLogin(form -> form
 						//.loginPage("/auth/login")            // redirect page login
 						.permitAll()
 				)
 				.logout(LogoutConfigurer::permitAll)
-
-		;
+				.sessionManagement(session -> session
+						.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+				)
+				.addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);
 
 		return http.build();
 	}
@@ -44,21 +54,6 @@ public class SecurityConfig {
 	public PasswordEncoder passwordEncoder() {
 		return PasswordEncoderFactories.createDelegatingPasswordEncoder();
 	}
-
-//	/**
-//	 * Test user auth
-//	 */
-//	@Bean
-//	public UserDetailsService userDetailsService() {
-//		UserDetails user =
-//				User.withDefaultPasswordEncoder()
-//						.username("user")
-//						.password("password")
-//						.roles("USER")
-//						.build();
-//
-//		return new InMemoryUserDetailsManager(user);
-//	}
 
 	/**
 	 * Database user auth
@@ -81,10 +76,10 @@ public class SecurityConfig {
 	}
 
 	@Bean
-	CorsConfigurationSource corsConfigurationSource() {
+	UrlBasedCorsConfigurationSource corsConfigurationSource() {
 		CorsConfiguration configuration = new CorsConfiguration();
 
-		configuration.setAllowedOrigins(List.of("*"));
+		configuration.setAllowedOrigins(List.of("http://localhost:5173"));
 		configuration.setAllowedMethods(List.of("GET","POST"));
 		configuration.setAllowedHeaders(List.of("authorization","content-Type", "x-auth-token"));
 
