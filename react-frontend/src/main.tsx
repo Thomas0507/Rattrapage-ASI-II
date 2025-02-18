d76e54674a04484f6de1effef1af3600d7c037f5
Thomas0507 <marin.f.thomas@gmail.com>
2025-02-18 22:12:02 +0100

    feat: Added typescript

--- react-frontend/src/main.tsx
+++ react-frontend/src/main.tsx
@@ -2,16 +2,17 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from "react-router-dom";
 import './index.css'
-import App from './App.jsx'
-import { AuthProvider } from "./hooks/useAuth";
-import Header from './pages/Header';
+import App from './App.js'
+import { AuthProvider } from "./hooks/useAuth.jsx";
+import Header from './pages/Header.js';
 
 import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
+import React from 'react';
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById('root') as HTMLElement).render(
   <StrictMode>
       <BrowserRouter>
         <AuthProvider>
