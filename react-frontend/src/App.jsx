417b433f03a2b8e8159c23c4fdeb226031760663
Thomas0507 <marin.f.thomas@gmail.com>
2025-02-18 19:38:57 +0100

    feat: Protected route & basic routing

--- react-frontend/src/App.jsx
+++ react-frontend/src/App.jsx
@@ -1,25 +1,32 @@
-import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from "react-router-dom";
+import { createBrowserRouter, createRoutesFromElements, Route, Routes, RouterProvider } from "react-router-dom";
 import Login from './pages/auth/Login';
 import Register from './pages/auth/Register';
 import Header from './pages/Header';
 import Home from './pages/Home';
-
-const router = createBrowserRouter(
-  createRoutesFromElements(
-    <Route path="/" element={<Header />}>
-      <Route index element={<Home />} />
-      <Route path="login" element={<Login />} />
-      <Route path="register" element={<Register />} />
-    </Route>
-  )
-);
+import { AuthProvider } from "./hooks/useAuth";
+import { ProtectedRoute } from "./components/ProtectedRoute";
+import { HomeLayout } from "./layouts/HomeLayout";
+import SelectScreen from "./pages/app/SelectScreen";
+import CardScreen from "./pages/app/CardScreen";
+import { ProtectedLayout } from "./layouts/ProtectedLayout";
 
 function App({}) {
 
   return (
-    <>
-      <RouterProvider router={router}/>
-    </>
+    <Routes>
+      {/* public routes */}
+      <Route element={<HomeLayout/>}>
+        <Route path="/" element = {<Home/>}/>
+        <Route path="/login" element = {<Login/>}/>
+        <Route path="/register" element = {<Register/>}/>
+      </Route>
+      {/* private routes */}
+      <Route path="/app" element={<ProtectedLayout/>}>
+        <Route path="main" element={<SelectScreen/>}/>
+        <Route path="cards" element={<CardScreen/>}/>
+      </Route>
+
+    </Routes>
   );
 }
 
---
+++ react-frontend/src/pages/app/SelectScreen.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const SelectScreen = () => {
+    return (
+        <div>
+            SelectScreen content
+        </div>
+    )
+};
+
+export default SelectScreen;
