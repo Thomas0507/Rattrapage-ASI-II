4b68f336e936f2765ec96adc6bc12ab1720a9034
Thomas0507 <marin.f.thomas@gmail.com>
2025-02-19 21:56:38 +0100

    feat: Enable CORS - Fully functionnal sign up

--- asi-ii/src/main/java/com/projet_asi_ii/asi_ii/controllers/TestController.java
+++ asi-ii/src/main/java/com/projet_asi_ii/asi_ii/controllers/TestController.java
@@ -5,12 +5,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-
 @RequestMapping("test")
-
+@RestController
 public class TestController {
 
-    @GetMapping(name = "/test")
+    @GetMapping("/test")
     public ResponseEntity<String>testFunction() {
         return ResponseEntity.ok("test");
     }
