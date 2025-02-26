f7d98f18f7d635fbae40cdcbf907b2166813b0d2
OumaimaRTAYLI <oumaima.rtayli@gmail.com>
2025-02-26 21:18:35 +0100

    conversation in private routing to be tested

--- react-frontend/src/App.tsx
+++ react-frontend/src/App.tsx
@@ -30,6 +30,7 @@ function App({ }) {
         <Route path="/" element = {<Home/>}/>
         <Route path="/login" element = {<Login/>}/>
         <Route path="/register" element = {<Register/>}/>
+
       </Route>
 
       {/* private routes */}
