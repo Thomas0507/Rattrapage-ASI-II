968a8f09f71a42490cc0d9ff3cfb471e3bedc0c6
Thomas0507 <marin.f.thomas@gmail.com>
2025-02-28 19:55:25 +0100

    feat: display banner front-side

--- react-frontend/src/pages/app/summon/SummonPage.tsx
+++ react-frontend/src/pages/app/summon/SummonPage.tsx
@@ -1,5 +1,5 @@
 import { Box, Button, Container, MobileStepper, Pagination, Stack } from "@mui/material"
-import React from "react"
+import React, { useEffect, useState } from "react"
 import BannerCard from "../../../components/BannerCard";
 import { Banner } from "../../../models/Banner";
 import { Card } from "../../../models/Card";
@@ -7,36 +7,69 @@ import NavigateNextIcon from '@mui/icons-material/NavigateNext';
 import ArrowBackIosIcon from '@mui/icons-material/ArrowBackIos';
 
 import '../../../css/SummonPage.css'
+import { getOptionsByRequestType, RequestType } from "../../../hooks/RequestBuilder";
+import Loader from "../../Loader";
 
 
 export default function SummonPage() {
 
-    const [activeStep, setActiveStep] = React.useState(0);
+    
+
+    const [activeStep, setActiveStep] = useState(0);
+    const [data, setData] = useState([]);
+    const [error, setError] = useState(false);
+    const [isLoading, setLoading] = useState(true); 
 
     const handleNext = () => {
         setActiveStep((prevActiveStep) => prevActiveStep + 1);
       };
     
-      const handleBack = () => {
+    const handleBack = () => {
         setActiveStep((prevActiveStep) => prevActiveStep - 1);
-      };
+    };
 
-    const data = [new Banner(1, 'test', 'desc', 0, true, new Date(), new Date(), true, "https://shared.fastly.steamstatic.com/store_item_assets/steam/apps/1623730/c961ac047294838bfe382bf193ade5c64b19e573/capsule_616x353.jpg?t=1737094038", [new Card
-        (
-            1, "Lamball", "A simple sheep trying his best", "https://paldex.io/cdn-cgi/image/format=auto,width=300/images/pal-icons/T_SheepBall_icon_normal.png", 1, 1, "Neutral"
-        )
-    ], [new Card(
-        1, "Lamball2", "A simple sheep trying his best", "https://paldex.io/cdn-cgi/image/format=auto,width=300/images/pal-icons/T_SheepBall_icon_normal.png", 1, 1, "Neutral"
-    )]),
-    new Banner(2, 'test2', 'desc2', 0, true, new Date(), new Date(), true, "https://shared.fastly.steamstatic.com/store_item_assets/steam/apps/1623730/c961ac047294838bfe382bf193ade5c64b19e573/capsule_616x353.jpg?t=1737094038", [new Card
-        (
-            1, "Lamball", "A simple sheep trying his best", "https://paldex.io/cdn-cgi/image/format=auto,width=300/images/pal-icons/T_SheepBall_icon_normal.png", 1, 1, "Neutral"
-        )
-    ], [new Card(
-        1, "Lamball2", "A simple sheep trying his best", "https://paldex.io/cdn-cgi/image/format=auto,width=300/images/pal-icons/T_SheepBall_icon_normal.png", 1, 1, "Neutral"
-    )])
-];
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const response = await fetch(`http://localhost:8081/banner`, getOptionsByRequestType(RequestType.GET, {}) )
+                if (!response.ok) {
+                    throw new Error(`Error: $(response.statusText)`);
+                }
+                // todo: handle banners not found
+                const result = await response.json();
+                setData(result);
+            } catch(err) {
+                setError(err.message)
+            } finally {
+                setLoading(false);
+            }
+        };
+        fetchData();
+    }, []);
 
+
+
+
+
+
+// mock data
+//     const data = [new Banner(1, 'test', 'desc', 0, true, new Date(), new Date(), true, "https://shared.fastly.steamstatic.com/store_item_assets/steam/apps/1623730/c961ac047294838bfe382bf193ade5c64b19e573/capsule_616x353.jpg?t=1737094038", [new Card
+//         (
+//             1, "Lamball", "A simple sheep trying his best", "https://paldex.io/cdn-cgi/image/format=auto,width=300/images/pal-icons/T_SheepBall_icon_normal.png", 1, 1, "Neutral"
+//         )
+//     ], [new Card(
+//         1, "Lamball2", "A simple sheep trying his best", "https://paldex.io/cdn-cgi/image/format=auto,width=300/images/pal-icons/T_SheepBall_icon_normal.png", 1, 1, "Neutral"
+//     )]),
+//     new Banner(2, 'test2', 'desc2', 0, true, new Date(), new Date(), true, "https://shared.fastly.steamstatic.com/store_item_assets/steam/apps/1623730/c961ac047294838bfe382bf193ade5c64b19e573/capsule_616x353.jpg?t=1737094038", [new Card
+//         (
+//             1, "Lamball", "A simple sheep trying his best", "https://paldex.io/cdn-cgi/image/format=auto,width=300/images/pal-icons/T_SheepBall_icon_normal.png", 1, 1, "Neutral"
+//         )
+//     ], [new Card(
+//         1, "Lamball2", "A simple sheep trying his best", "https://paldex.io/cdn-cgi/image/format=auto,width=300/images/pal-icons/T_SheepBall_icon_normal.png", 1, 1, "Neutral"
+//     )])
+// ];
+
+    if (isLoading) return <Loader />
     return (
     <Container maxWidth="lg">
         <MobileStepper
@@ -60,5 +93,5 @@ export default function SummonPage() {
     />
     <BannerCard banner={data[activeStep]} />
     </Container>
-    )
+    );
 }
