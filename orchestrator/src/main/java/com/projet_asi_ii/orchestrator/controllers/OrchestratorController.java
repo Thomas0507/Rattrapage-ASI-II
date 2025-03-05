b5b157a56c8eaae607ea47c8bfedb07bf7f9a679
remi <remitvb@gmail.com>
2025-03-05 21:19:37 +0100

    feat: add prompt request to the orchestrator

--- orchestrator/src/main/java/com/projet_asi_ii/orchestrator/controllers/OrchestratorController.java
+++ orchestrator/src/main/java/com/projet_asi_ii/orchestrator/controllers/OrchestratorController.java
@@ -1,12 +1,11 @@
 package com.projet_asi_ii.orchestrator.controllers;
 
+import com.projet_asi_ii.orchestrator.requests.PromptRequest;
 import com.projet_asi_ii.orchestrator.services.OrchestratorService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestHeader;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import javax.smartcardio.Card;
 import java.util.UUID;
 
 @RestController
@@ -17,9 +16,9 @@ public class OrchestratorController
 	@Autowired
 	private OrchestratorService orchestratorService;
 
-	@GetMapping("/generateCard")
-	public String generateCard(@RequestHeader("Authorization") String bearerToken) {
-		orchestratorService.sendRequests(UUID.randomUUID(), bearerToken);
+	@PostMapping("/generateCard")
+	public String generateCard(@RequestHeader("Authorization") String bearerToken, @RequestBody PromptRequest promptRequest) {
+		orchestratorService.sendRequests(UUID.randomUUID(), bearerToken, promptRequest);
 
 		return "Request sent";
 	}
