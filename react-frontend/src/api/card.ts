ed0e1198c6dffd753ed4d16d5ce207a8dbfc9989
raghad <raghad.alhomsi2225@gmail.com>
2025-03-06 23:18:14 +0100

    image gen service

--- react-frontend/src/api/card.ts
+++ react-frontend/src/api/card.ts
@@ -1,16 +1,13 @@
+import { getOptionsByRequestType, RequestType } from "../hooks/RequestBuilder";
 
-export const generateCard = async (userId: string, prompt: string) => {
-    const response = await fetch('http://localhost:8081/card', {
-      method: "POST",
-      headers: {
-        'Access-Control-Request-Method': 'POST',
-        'Origin': 'http://localhost:5173',
-        'Accept': 'application/json',
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({ userId, prompt }),
-    });
-  
+export const generateCard = async (prompt: string) => {
+    const response = await fetch('http://localhost:8081/cards/generateCard', getOptionsByRequestType(RequestType.POST,{
+      "promptRequest": prompt
+    }));
+    if (!response.ok) {
+      throw new Error(`Error: $(response.statusText)`);
+    }
     const data = await response.json();
+    console.log(data);
     return data;
   };
