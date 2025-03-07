e55a212673998691a1e9661176583c7fae33ec74
OumaimaRTAYLI <oumaima.rtayli@gmail.com>
2025-03-07 23:15:11 +0100

    price added to cards

--- react-frontend/src/pages/transaction/Buy.tsx
+++ react-frontend/src/pages/transaction/Buy.tsx
@@ -3,12 +3,16 @@ import { Container, Typography } from '@mui/material';
 import CardListComponent from '../../components/CardListComponent';
 import { getOptionsByRequestType, RequestType } from '../../hooks/RequestBuilder';
 import { Card } from '../../models/Card';
+import { useAuth } from '../../hooks/useAuth';
+
 
 
 const Buy: React.FC = () => {
   const [cards, setCards] = React.useState<Card[]>([]);
   const [loading, setLoading] = React.useState<boolean>(true);
   const [error, setError] = React.useState<string | null>(null);
+
+  const { user } = useAuth();
 
   React.useEffect(() => {
     const fetchCards = async () => {
@@ -30,11 +34,23 @@ const Buy: React.FC = () => {
   }, []);
 
   const handleBuy = async (cardId: number) => {
+    const selectedCard = cards.find(card => card.id === cardId);
+    if (!selectedCard) {
+      alert("Card not found!");
+      return;
+    }
     try {
       const response = await fetch("http://localhost:8081/transaction", {
         method: "POST",
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify({ cardId, transactionType: "BUY" }),
+        headers: { 
+          "Content-Type": "application/json",
+          "Authorization": `Bearer ${user}` 
+        },
+        body: JSON.stringify({ 
+          username: user.username,          // utilisateur connecté
+          amount: selectedCard.price,       // prix venant de CardEntity
+          cardId: selectedCard.id, 
+        }),
       });
 
       if (!response.ok) {
